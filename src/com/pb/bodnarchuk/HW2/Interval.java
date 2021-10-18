a66c5aa65e7ec74f3e8b2ebb6cd006e6aff8c3e9
Bodnarchuk Maxim <bodnarchukm21@gmail.com>
2021-10-18 22:36:13 +0300

    init commit

--- src/com/pb/bodnarchuk/HW2/Interval.java
+++ src/com/pb/bodnarchuk/HW2/Interval.java
@@ -8,14 +8,14 @@ int a;
         System.out.println("введіть число від 0-100");
         Scanner peremenna = new Scanner(System.in);
 a=peremenna.nextInt();
-if (a>=0&&a<=14){
+if (a>=0&&a<=14) {
     System.out.println("діапазон 0-14");
-}else if (a<=35) {
+}else if (a>15&&a<=35) {
     System.out.println("діапазон 15-35");
-}else if (a<=50){
-    System.out.println("діапазон 50");
-}else if (a<=100){
-    System.out.println("діапазон 100");
+}else if (a>36&&a<=50){
+    System.out.println("діапазон 36-50");
+}else if (a>51&&a<=100){
+    System.out.println("діапазон 51-100");
 }else {
     System.out.println("нема числа в діапазоні");
 }
