425f398d33cda165b2a610057de99eba589be3f8
Bodnarchuk Maxim <bodnarchukm21@gmail.com>
2021-11-03 05:07:39 +0200

    init commit

--- src/HW5/takeBook.java
+++ src/HW5/takeBook.java
@@ -22,17 +22,14 @@ public class takeBook {
         System.out.println(reader2.getInfo());
         System.out.println(reader3.getInfo());
 
-        book1.takebook(reader1.getName());
-        book2.takebook(reader3.getName()  + book1.getName()   +    book2.getName()   +   book3.getName());
-        book3.takebook(reader2.getName()  +   book1.getName()  +
-                book1.getAuthorbook()   +   book1.getYear());
+        book1.takebook(reader1.getName()  + book1.getName() +"_" +"автора" +"_" + book1.getAuthorbook() +"_" + book2.getYear() ) ;
+        book2.takebook(reader3.getName()  + book3.getName()   +  "_"+  book3.getAuthorbook()   + "_" +  book3.getYear());
+        book3.takebook(reader2.getName()  +   book2.getName()  + "_" + book2.getAuthorbook()   + "_"  + book2.getYear());
         System.out.println();
 
-        book1.returnBook(reader1.getName());
-        book2.returnBook(reader1.getName() + book1.getName()   +    book2.getName()   +   book3.getName());
-        book3.returnBook(reader3.getName() +   book1.getName()  +
-                book1.getAuthorbook()   +   book1.getYear() + book2.getName() + book2.getAuthorbook()   +   book2.getYear()
-                + book3.getName() +  book3.getAuthorbook()   +   book3.getYear());
+        book1.returnBook(reader1.getName() + book1.getName() +"_" +"автора" +"_" + book1.getAuthorbook() +"_" + book2.getYear());
+        book2.returnBook(reader1.getName() + book3.getName()   +  "_"+  book3.getAuthorbook()   + "_" +  book3.getYear());
+        book3.returnBook(reader3.getName() +   book2.getName()  + "_" + book2.getAuthorbook()   + "_"  + book2.getYear());
         System.out.println();
     }
 }
